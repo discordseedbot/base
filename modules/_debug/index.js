1e9e1657cfbd6bbc3a16dd5e0507b6c0a92b2182
Jyles Coad-Ward <jyles@dariox.club>
2020-09-14 11:47:33 +0800

    Update index.js

--- modules/_debug/index.js
+++ modules/_debug/index.js
@@ -3,15 +3,15 @@ module.exports = function() {
     // Print all Libaries
     //console.log(libr);
 
-    SB_Client.on('ready', async () => {
+    SB.client.on('ready', async () => {
         /*
-        console.log(SB_Client.guilds.cache.map(m => m.name).join("\n"))
+        console.log(SB.client.guilds.cache.map(m => m.name).join("\n"))
         let totalSize = {
             "members": 0,
             "guilds": 0,
             "channels": 0
         };
-        SB_Client.guilds.cache.forEach(m => {
+        SB.client.guilds.cache.forEach(m => {
             totalSize.members+=m.memberCount
             totalSize.guilds+=1
             totalSize.channels+=m.channels.cache.size
@@ -22,7 +22,7 @@ module.exports = function() {
 
     })
 
-    SB_Client.on('message', async message => {
+    SB.client.on('message', async message => {
         if (message.author.bot) return;
         console.log(`[${message.author.id}] - ${message.content}`)
     })
