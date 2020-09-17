ee0635a6890850d43fde699700f754f3f1de724a
Jyles Coad-Ward <jyles@dariox.club>
2020-09-17 12:36:18 +0800

    Update index.js

--- modules/_debug/index.js
+++ modules/_debug/index.js
@@ -1,27 +1,4 @@
 module.exports = function() {
-
-    // Print all Libaries
-    //console.log(libr);
-
-    SB.client.on('ready', async () => {
-        /*
-        console.log(SB.client.guilds.cache.map(m => m.name).join("\n"))
-        let totalSize = {
-            "members": 0,
-            "guilds": 0,
-            "channels": 0
-        };
-        SB.client.guilds.cache.forEach(m => {
-            totalSize.members+=m.memberCount
-            totalSize.guilds+=1
-            totalSize.channels+=m.channels.cache.size
-        })
-        console.log(totalSize)
-        delete(totalSize)
-        */
-
-    })
-
     SB.client.on('message', async message => {
         if (message.author.bot) return;
         console.log(`[${message.author.id}] - ${message.content}`)
