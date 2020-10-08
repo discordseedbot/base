87feb9a1112668dca56e08dd0a4cef2db6054986
Jyles Coad-Ward <jyles@dariox.club>
2020-10-08 19:02:26 +0800

    fix

--- .buildTools.js
+++ .buildTools.js
@@ -1,24 +1,21 @@
-module.exports.buildIncrement = function() {
+module.exports.buildIncrement = async function() {
 	try {
 		const fs = require("fs");
 		const ljf = require('load-json-file')
 
-		async function incrementPackage(){
-		    const file = await ljf("package.json");
-		    file.build.number++;
-		    var curDate = new Date();
-		    file.build.date = `${curDate.getFullYear()}_${curDate.getMonth()}_${curDate.getDate()}`;
-		    file.build.timestamp = Math.round(curDate.valueOf()/1000);
 
-		    fs.writeFile("./package.json", JSON.stringify(file,null,"\t"), function writeJSON(err) {
-		        if (err) return console.log(err);
-		        console.log("incremented package number");
-		    });
-		}
+	    const file = await ljf("package.json");
+	    file.build.number++;
+	    var curDate = new Date();
+	    file.build.date = `${curDate.getFullYear()}_${curDate.getMonth()}_${curDate.getDate()}`;
+	    file.build.timestamp = Math.round(curDate.valueOf()/1000);
 
-		incrementPackage()
+	    fs.writeFile("./package.json", JSON.stringify(file,null,"\t"), function writeJSON(err) {
+	        if (err) throw err;
+	        console.log("incremented package number");
+	    });
 	} catch (e) {
 		console.error(e);
-		process.exit(1)
+		process.exit(1);
 	}
 }
