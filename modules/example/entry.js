2dd2176b0e43ba4d1c080738f97027d6bd8977f7
Jyles Coad-Ward <jyles@dariox.club>
2021-03-25 12:03:26 +0800

    Created example module

--- modules/example/entry.js
+++ modules/example/entry.js
@@ -1,12 +1,20 @@
 /** @type {module:CoreModule.BotScript} */
 module.exports = {
+	/** @type {String[]} */
+	commands: ["ping"],
+
 	/** @type {module:CoreModule.onReady} */
 	onReady: async (Client,Storage) => {
-
+		
 	},
 
 	/** @type {module:CoreModule.onMessage} */
 	onMessage: async (Message,Storage) => {
-
+		switch (Message.command) {
+			case "ping":
+				var pingmsg = await message.channel.send("Pong!");
+				pingmsg.edit(`Pong! Took \`${Date.now() - pingmsg.createdTimestamp}ms\``);
+				break;
+		}
 	},
 }
