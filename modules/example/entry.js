4e201899043302595a03e2e9428ef3d13e89d493
Jyles Coad-Ward <jyles@dariox.club>
2021-03-25 10:02:59 +0800

    Example Module

---
+++ modules/example/entry.js
@@ -0,0 +1,12 @@
+/** @type {module:CoreModule.BotScript} */
+module.exports = {
+	/** @type {module:CoreModule.onReady} */
+	onReady: async (Client,Storage) => {
+
+	},
+
+	/** @type {module:CoreModule.onMessage} */
+	onMessage: async (Message,Storage) => {
+
+	},
+}
