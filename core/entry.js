d635aa0b3c8e4a79694a1090f4ea53ae07fb379f
Jyles Coad-Ward <jyles@dariox.club>
2021-03-25 12:02:20 +0800

    Create data folders if not found

--- core/entry.js
+++ core/entry.js
@@ -1,5 +1,18 @@
 console.log("Setting up Internal Structure");
 var SBcore = require("./corelib.js");
+const fs = require("fs");
+if (!fs.existsSync("./data/")) {
+	console.log(`[entry] Created Data Directory at "./data/"`);
+	fs.mkdirSync("./data/");
+	if (!fs.existsSync("./data/db/")) {
+		fs.mkdirSync("./data/db/");
+	console.log(`[entry] Created Database Directory at "./data/db"`);
+	}
+	if (!fs.existsSync("./data/cache.db3")) {
+		fs.copyFileSync("./core/defaultDatabase.db3","./data/cache.db3");
+		console.log(`[entry] Created Cache at "./data/cache.db3"`);
+	}
+}
 
 /**
  * Global SB Module
