5c1e4839f5a97c994681e34d9aa6518fb916dbe2
Jyles Coad-Ward <jyles@dariox.club>
2021-03-25 09:03:55 +0800

    Build Increment working

--- core/buildtools.js
+++ core/buildtools.js
@@ -1,4 +1,4 @@
-const { resolve } = require("path");
+const toolbox = require("tinytoolbox")
 
 /**
  * @module CoreBuildTools
@@ -14,19 +14,19 @@ module.exports =
 		return new Promise(async (res,rej)=>{
 			try {
 				const fs = require("fs");
+				var templateConfig = {
+					"name": "Discord Bot",
+					"version": "0.1",
+					"contributers": [ ],
+					"homepage": "https://example.com",
+					"contact": "username@example.com",
+					"build": {
+						"id": 0,
+						"timestamp": 0
+					}
+				};
 
 				if (!fs.existsSync("./seedbot.config.json")) {
-					var templateConfig = {
-						"name": "Discord Bot",
-						"version": "0.1",
-						"contributers": [ ],
-						"homepage": "https://example.com",
-						"contact": "username@example.com",
-						"build": {
-							"id": 0,
-							"timestamp": 0
-						}
-					};
 
 					try {
 						await fs.writeFileSync("./seedbot.config.json",JSON.stringify(templateConfig,null,'\t'));
@@ -36,10 +36,33 @@ module.exports =
 						process.exit(1);
 					}
 				} else {
-					const originalJSON = await require("./../seedbot.config.json");
+					var originalJSON = await require("./../seedbot.config.json");
 
 					originalJSON.build.id++;
 					originalJSON.build.timestamp = await Date.now();
+
+					// Check if fields are valid
+					if (originalJSON.name === undefined || typeof originalJSON.name !== "string") {
+						originalJSON.name = "Discord Bot";
+					}
+
+					if (originalJSON.version === undefined) {
+						originalJSON.version = templateConfig.version;
+					}
+
+					await toolbox.async.forEach(toolbox.JSON.toArray(originalJSON),async (mf)=>{
+						if (mf[0] !== "build" && (mf[1] === undefined || typeof templateConfig[mf[0]] !== typeof mf[1])) {
+							mf[1] = templateConfig[mf[0]];
+									console.log(`[buildtools -> increment()] Object in "seedbot.config.json" at '${mf[0]}' is not a supported type, setting to default value '${originalJSON[mf[0]]}'`);
+						} else if (mf[0] === "build") {
+							await toolbox.async.forEach(toolbox.JSON.toArray(originalJSON.build),(bmf)=>{
+								if (bmf[1] === undefined || typeof bmf[1] !== typeof originalJSON.build[bmf[0]]) {
+									bmf[1] = originalJSON.build[bmf[0]];
+									console.log(`[buildtools -> increment()] Object in "seedbot.config.json" at 'build.${bmf[0]}' is not a supported type, setting to default value '${originalJSON.build[bmf[0]]}'`);
+								}
+							})
+						}
+					})
 
 					originalJSON = await JSON.stringify(originalJSON,null,'\t');
 
