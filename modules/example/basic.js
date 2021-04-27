efbaba73e0c2d74b5c5159c4bfdad1ff62e7140a
Jyles Coad-Ward <jyles@dariox.club>
2021-04-27 10:36:19 +0800

    Nothing changed

---
+++ modules/example/basic.js
@@ -0,0 +1,23 @@
+/** @type {module:CoreModule.BotScript} */
+module.exports = {
+	/** @type {String[]} */
+	commands: ["ping","uptime"],
+
+	/** @type {module:CoreModule.onReady} */
+	onReady: async (Client,Storage) => {
+		
+	},
+
+	/** @type {module:CoreModule.onMessage} */
+	onMessage: async (Message,Storage) => {
+		switch (Message.command) {
+			case "ping":
+				var pingmsg = await Message.channel.send("Calculating...");
+				pingmsg.edit(`:satellite: Local ${pingmsg.createdTimestamp - Message.createdTimestamp}ms. \n:page_with_curl: API ${Math.round(SB.client.ws.ping)}ms`);
+				break;
+			case "uptime":
+				Message.channel.send(`I've been up since ${Message.client.readyAt}`);
+				break;
+		}
+	},
+}
